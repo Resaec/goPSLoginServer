4a5ab81ff1ee4df4cd22e4ef83c001570c9eac7f
Resaec <resaecscript@googlemail.com>
2024-03-21 22:10:07 +0100

    Dropping admin and world files and references

--- main.go
+++ main.go
@@ -1,34 +1,17 @@
 package main
 
 import (
-	"goPSLoginServer/admin"
 	"goPSLoginServer/login"
 	"goPSLoginServer/utils"
-	"goPSLoginServer/world"
 )
-
-/*
-
-Ports
-
-UDP
-
-51000/udp Login
-51001/udp World
-
-TCP / Admin
-
-51002/tcp
-
-*/
 
 func main() {
 
 	utils.GlobalWaitGroup.Add(3)
 
+	utils.GlobalWaitGroup.Add(1)
+
 	go login.HandleLogin(51000)
-	go world.HandleWorld(51001)
-	go admin.HandleAdmin(51002)
 
 	utils.GlobalWaitGroup.Wait()
 }
